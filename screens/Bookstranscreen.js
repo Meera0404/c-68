5ad275ba538be3e22bea797fd35942917a21b4a9
Meera0404 <76114341+Meera0404@users.noreply.github.com>
2021-07-28 20:57:05 +0530

    Add files via upload

---
+++ screens/Bookstranscreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text,View } from "react-native";
+
+export default class Transactionscreen extends React.Component{
+  render(){
+      return(
+          <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+           <Text> Isuue or Return </Text>
+           </View>
+      )
+  }  
+}
